a7edbc1963c92e2b9fe24902d3a676a44a980d3a
sainiajay <saini.ajay172@gmail.com>
2020-08-13 11:54:16 +0530

    Add Env files

--- src/components/Login/spotify.js
+++ src/components/Login/spotify.js
@@ -2,7 +2,6 @@ const getAccessUrl = () => {
     const authEndpoint = 'https://accounts.spotify.com/authorize'
     // Replace with your app's client ID, redirect URI and desired scopes
     const clientId = '7a5bd131e8184ad78d98ce4ad2952e7d'
-    const redirectUri = 'http://localhost:8000/'
     const scopes = [
         'user-read-currently-playing',
         'user-read-recently-played',
@@ -12,7 +11,7 @@ const getAccessUrl = () => {
     ]
     const url = new URL(authEndpoint)
     url.searchParams.set('client_id', clientId)
-    url.searchParams.set('redirect_uri', redirectUri)
+    url.searchParams.set('redirect_uri', process.env.SPOTIFY_CALLBACK)
     url.searchParams.set('scope', scopes.join(' '))
     url.searchParams.set('response_type', 'token')
     return url.href
