20333dd179c629df469b7836001da4e2c756a75d
sainiajay <saini.ajay172@gmail.com>
2020-08-28 22:59:33 +0530

    Sidebar menu Revamp

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,19 +1,13 @@
 import React from "react"
-import style from "./index.module.css"
+import style from "./Sidebar.module.css"
 import { useStateValue } from "./../../context/StateProvider"
 import Logo from "./../Logo";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHome, faSearch, faMusic } from "@fortawesome/free-solid-svg-icons"
-
-const SidebarOption = ({ option = "test", icon }) => {
-    return (
-      <div className={style.SidebarOption}>
-        {icon && <FontAwesomeIcon icon={icon} size="2x" className={style.SidebarOptionIcon} />}
-        {icon? <h4>{option}</h4> : <p>{option}</p>}
-      </div>
-    )
-}
+import HomeIcon from "@material-ui/icons/HomeOutlined"
+import SearchIcon from "@material-ui/icons/Search"
+import LibraryIcon from "@material-ui/icons/LibraryMusicOutlined"
+import AddIcon from "@material-ui/icons/Add"
+import FavoriteIcon from "@material-ui/icons/Favorite"
 
 function Sidebar() {
   const [{ playlists }] = useStateValue()
@@ -21,15 +15,33 @@ function Sidebar() {
   return (
     <div className={style.SidebarContainer}>
       <div className={style.Sidebar}>
-        <Logo />
-        <SidebarOption option="Home" icon={faHome}/>
-        <SidebarOption option="Search" icon={faSearch}/>
-        <SidebarOption option="Your Library" icon={faMusic}/>
-        <br />
-        <strong className={style.Title}>PLAYLISTS</strong>
+        <div className={style.SidebarLogo}>
+          <Logo />
+        </div>
+        <div className={style.SidebarOption} data-active="true">
+          <HomeIcon className={style.SidebarOptionIcon}/>
+          <h4>Home</h4>
+        </div>
+        <div className={style.SidebarOption}>
+          <SearchIcon className={style.SidebarOptionIcon}/>
+          <h4>Search</h4>
+        </div>
+        <div className={style.SidebarOption}>
+          <LibraryIcon className={style.SidebarOptionIcon}/>
+          <h4>Your Library</h4>
+        </div>
+        <h3 className={style.Title}>PLAYLISTS</h3>
+        <div className={style.SidebarOption}>
+          <AddIcon fontSize="large" className={style.AddIcon}/>
+          <h4>Create Playlist</h4>
+        </div>
+        <div className={style.SidebarOption}>
+          <FavoriteIcon fontSize="large" className={style.FavoriteIcon}/>
+          <h4>Liked Songs</h4>
+        </div>
         <hr />
         {
-          playlists?.items?.map(playlist => <SidebarOption key={playlist.id} option={playlist.name} />)
+          playlists?.items?.map(playlist => <a className={style.Playlist} key={playlist.id} href="#">{playlist.name}</a> )
         }
       </div>
     </div>
